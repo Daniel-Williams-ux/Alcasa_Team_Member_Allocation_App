766e07e16e930871266ebde39341b80da3735dd8
Daniel Williams <williamsoligodaniel@gmail.com>
2022-12-27 11:04:22 +0000

    1. Create an on-click function for event listeners. 2. Create a function that compares employee id to an event and compares the team name with a selected team. 3.  Create a CSS class name function that toggles over the employees' cards. 4. Add a CSS class of box shadow

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -1,4 +1,4 @@
- import React from 'react'
+import React from 'react'
 import { useState } from 'react';
 import femaleProfile from './images/femaleProfile.jpg';
 import maleProfile from './images/maleProfile.jpg';
@@ -95,6 +95,14 @@ const Employees = () => {
     console.log(event.target.value);
     setSelectedTeam(event.target.value);
   }
+
+  function handleEmployeeCardClick(event) {
+    const transformedEmployees = employees.map((employee) => employee.id === parseInt(event.currentTarget.id)
+      ? (employee.teamName === selectedTeam) ? {...employee, teamName: ''} : {...employee, teamName: selectedTeam} :employee);
+
+    setEmployees(transformedEmployees);
+  }
+  
   return (
     <main className="container">
        <div className="row justify-content-center mt-3 mb-3">
@@ -112,7 +120,7 @@ const Employees = () => {
           <div className="card-collection">
       {
        employees.map((employee) => (
-         <div id={employee.id} className="card m-2" style={{cursor: "pointer"}}>
+         <div id={employee.id} className={(employee.teamName === selectedTeam ? 'card m-2 standout' : 'card m-2')} style={{cursor: 'pointer'}} onClick={handleEmployeeCardClick}>
   {(employee.gender === 'male') ? <img src={maleProfile} className="card-img-top"/> : <img src={femaleProfile} className="card-img-top"/>}
            <div className="card-body">
              <h5 className="card-title">Full Name: {employee.fullName}</h5>
