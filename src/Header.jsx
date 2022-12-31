937941e2ba4422e13de2b1b0490237805ffa63b8
Daniel Williams <williamsoligodaniel@gmail.com>
2022-12-31 11:42:12 +0000

    use bootstrap to style the header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,13 @@
 const Header = ({selectedTeam, teamMemberCount}) => {
   
   return (
-    <header>
-      <h1>Team Member Allocaion</h1>
-      <h3>{selectedTeam} has {teamMemberCount} members</h3>
+    <header className="container">
+      <div className="row justify-content-center mt-3 mb-4">
+        <div className="col-8">
+          <h1>Team Member Allocaion</h1>
+          <h3>{selectedTeam} has {teamMemberCount} members</h3>
+        </div>
+      </div>
     </header>
   )
   
