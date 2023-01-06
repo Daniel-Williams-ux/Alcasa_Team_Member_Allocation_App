c99b2b2d4a76278c09cb718bb36af6f00e345537
Daniel Williams <williamsoligodaniel@gmail.com>
2023-01-06 10:04:15 +0000

    correct navbar bootstrap styling

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -2,8 +2,8 @@ import {Link} from 'react-router-dom';
 
 const Nav = () => {
   return (
-    <nav className="navbar navbar-expanded-lg navbar-light bg-light">
-      <ul className="navbarinav me-auto mb-2 mb-lg-g">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <ul className="navbar nav me-auto mb-2 mb-lg-g">
         <li className="nav-item">
           <Link className="nav-link" to ="/">Home</Link>
         </li>
